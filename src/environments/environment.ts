13cda4d41f40edf7a12c8319ee205205b1ee2c98
yogibagus <yogibaguskarunia@gmail.com>
2024-08-19 18:28:30 +0700

    change env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production      : true,
-    // apiURL          : "https://linkedin-roaster.onrender.com/api",
-    apiURL          : "http://localhost:3000/api",
+    apiURL          : "https://linkedin-roaster.onrender.com/api",
+    // apiURL          : "http://localhost:3000/api",
 };
